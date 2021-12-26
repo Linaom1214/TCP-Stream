1649b90379fc4e725751558a53e167955cc1b04f
Linaom1214 <60921095+Linaom1214@users.noreply.github.com>
2021-12-26 22:28:55 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,47 +1,44 @@
 import socket
 import cv2
-import numpy as np
 import time
+import numpy as np
 import pickle
 
-def send_file_2_client(new_client_socket, frame):
-    frame =  frame.tobytes()
-    data = {
-        'image':frame,
-        'position':10
-    }
-    data = pickle.dumps(data)
-    new_client_socket.send(str.encode(str(len(data)).ljust(16)))
-    new_client_socket.send(data)
-    receive = new_client_socket.recv(1024)
-    print(receive)
-
-
+def recvall(sock, count):
+    buf = b''#buf是一个byte类型
+    while count:
+        #接受TCP套接字的数据。数据以字符串形式返回，count指定要接收的最大数据量.
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
 
 def main():
-    #(创建套接字 socket)
-    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #(绑定本地信息 bind)  客户端不绑定
-    tcp_server_socket.bind(("", 8080))
-    # 响铃模式(让默认的套接字由主动变为被动 listen)
-    tcp_server_socket.listen(128)
-    cap = cv2.VideoCapture(0)
-    rect, frame = cap.read()
-    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),15]
+    
+    dest_ip = ""
+    dest_port = 8080
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind((dest_ip,dest_port)) # 绑定本机IP
+    tcp_socket.listen(128) 
+    new_client_socket, client_addr = tcp_socket.accept()
     while True:
-        rect, frame = cap.read()
-        # 4. 等待别人的电话到来(等待客户端的链接 accept)
-        # new_client_socket 通信套接字
-        new_client_socket, client_addr = tcp_server_socket.accept()
-        # 5. 调用发送文件函数，完成为客户端服务
-        # frame  = cv2.resize(frame,(128,128))
-        frame = np.array(frame,dtype=np.uint8)
-        _, frame  = cv2.imencode('.jpg', frame, encode_param)
-        send_file_2_client(new_client_socket, frame)
-        # 6. 关闭套接字
-        new_client_socket.close()
-        # time.sleep(0.1)
-    tcp_server_socket.close()
+        start =time.time()
+        length = recvall(new_client_socket,16)#获得图片文件的长度,16代表获取长度
+        stringData = recvall(new_client_socket, int(length))#根据获得的文件长度，获取图片文件
+        stringData = pickle.loads(stringData)
+        stringData = stringData['image']
+        data = np.frombuffer(stringData, np.uint8)#将获取到的字符流数据转换成1维数组
+        decimg=cv2.imdecode(data,cv2.IMREAD_COLOR)#将数组解码成图像
+        canny = cv2.Canny(decimg, 50, 150)
+        cv2.imshow("img",canny)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        use_time = time.time() - start
+        fps = 1 / use_time     
+        new_client_socket.send(str(fps).encode('utf-8'))
+    tcp_socket.close()
+    
 
 
 if __name__ == "__main__":
